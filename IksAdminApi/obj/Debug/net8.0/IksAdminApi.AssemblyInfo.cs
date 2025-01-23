4f38db5d2ab988f7ff8f9f90c9b3a3dccc9edbb7
iksix <pocetikyt@gmail.com>
2025-01-23 17:50:45 +0700

    1

--- IksAdminApi/obj/Debug/net8.0/IksAdminApi.AssemblyInfo.cs
+++ IksAdminApi/obj/Debug/net8.0/IksAdminApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IksAdminApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7829113649a788071ea8a402e0ae053a33d1f36a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a78b6a94391d94e258845e2673ae912a8619df8")]
 [assembly: System.Reflection.AssemblyProductAttribute("IksAdminApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IksAdminApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
